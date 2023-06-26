6226db9b648a7b082577b498fefc4d8fd9bba8d6
GiammarinoJL <GiammarinoJL@Vt-GIAMMARINOJL.valueteam.com>
2023-06-26 10:40:06 +0200

    Inserimento metodo PostConstruct

--- ApplicationLogger/src/main/java/com/example/demo/logging/LoggerWrapper.java
+++ ApplicationLogger/src/main/java/com/example/demo/logging/LoggerWrapper.java
@@ -5,35 +5,41 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.consumer.LogMessage;
 import com.example.demo.producer.RabbitProducer;
 
-@Component
+import jakarta.annotation.PostConstruct;
+
+@Service
 @Scope("prototype")
 public class LoggerWrapper {
 	private Logger log;
 	private LogMessage logMessage;
 	@Autowired RabbitProducer producer;
 	
-	@Value("{spring.application.name}")
+	@Value("${spring.application.name}")
 	private String appName;
 	
 	private <T> LoggerWrapper(Class<T> c, String functionality) {
 		log= Logger.getLogger(c.getSimpleName());
-		
+	
 		logMessage= new LogMessage();
-		logMessage.setApplicationName(appName);
 		logMessage.setClassName(c.getSimpleName());
 		logMessage.setFunctionality(functionality);
 		logMessage.setModuleName(c.getPackageName());
 		
 	}
 	
-	public <T> LoggerWrapper getLogger(Class<T> c, String functionality) {
-		return new LoggerWrapper(c, functionality);
+	@PostConstruct
+	public void init() {
+		logMessage.setApplicationName(appName);
 	}
+	
+	//public <T> LoggerWrapper getLogger(Class<T> c, String functionality) {
+	//	return new LoggerWrapper(c, functionality);
+	//}
 	
 	public void info(String message, String transactionId) {
 		log.info(message);
